465f37a96dc773f06e6e7b502338763f9a5b49be
Joshua Handschin <41125028+JHandschin@users.noreply.github.com>
2021-03-06 00:34:41 -0700

    Add files via upload

--- p5.py
+++ p5.py
@@ -7,12 +7,15 @@ from bs4 import BeautifulSoup
 import re
 
 # %%
+# Get the html
 html = requests.get(
     'https://www.hotslogs.com/Sitewide/ScoreResultStatistics?Tab2=roleTabBruiser'
 )
 soup = BeautifulSoup(html.text, 'html5lib')
 
 # %%
+# sift the the html table to get the rows and columns
+# to build the DataFrame
 rows = []
 columns = []
 
@@ -25,10 +28,16 @@ for row in soup.find_all('tr'):
 
 rows
 # %%
+# Build the DataFrame
 headers = ['junk1', 'hero', 'games', 'winPercent', 'junk2',  'takedownDeathRatio', 'takedowns', 'kills', 'deaths', 'heroDamage', 'siegeDamage', 'healing', 'selfHeal', 'damageTaken', 'expGain']
 df = pd.DataFrame(data=rows, columns=headers)
 df = df.drop(df.index[[0,1]])
 df = df.drop(df.index[90:])
+df
+# %%
+# Save to csv 
+df.to_csv(r'hotsData.csv', index=False, header=True) # saves to local working directory
+df = pd.read_csv('hotsData.csv') # reload from file in local working directory
 
 #%%
 df
